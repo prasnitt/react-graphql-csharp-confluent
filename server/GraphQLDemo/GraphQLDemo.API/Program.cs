5e039988edefafe18e9dd641f0c86ec1c27e964e
Prashant Agrawal <prashant.agrawal@2degrees.nz>
2025-04-23 23:59:04 +1200

    add aws app runner graphQL

--- server/GraphQLDemo/GraphQLDemo.API/Program.cs
+++ server/GraphQLDemo/GraphQLDemo.API/Program.cs
@@ -40,12 +40,21 @@ builder.Services.Configure<SchemaRegistryConfig>(builder.Configuration.GetSectio
 builder.Services.AddSingleton<ISchemaRegistryClient>(sp =>
 {
     var config = sp.GetRequiredService<IOptions<SchemaRegistryConfig>>();
+    if (string.IsNullOrEmpty(config.Value.BasicAuthUserInfo))
+    {
+        config.Value.BasicAuthUserInfo = Environment.GetEnvironmentVariable("CONFLUENT_SCHEMA_REGISTRY_AUTH");
+    }
     return new CachedSchemaRegistryClient(config.Value);
 });
 
 builder.Services.AddSingleton<IProducer<String, ChatMessage>>(sp =>
 {
     var config = sp.GetRequiredService<IOptions<ProducerConfig>>();
+    if (string.IsNullOrEmpty(config.Value.SaslPassword))
+    {
+        config.Value.SaslPassword = Environment.GetEnvironmentVariable("CONFLUENT_SASL_PASSWORD");
+    }
+
     var schemaRegistryClient = sp.GetRequiredService<ISchemaRegistryClient>();
 
     // Debugging

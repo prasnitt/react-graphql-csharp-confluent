ee78685b584a065dbb68997f827198404d9c2534
Prashant Agrawal <prashant.agrawal@2degrees.nz>
2025-04-25 01:25:59 +1200

    amend

--- server/GraphQLDemo/ChatHub/ec2deploy/deploy.sh
+++ server/GraphQLDemo/ChatHub/ec2deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e  # Ex
 
-cd git/prasnitt/react-graphql-csharp-confluent
+cd /home/ec2-user/react-graphql-csharp-confluent
 git reset --hard
-git pull origin main
+git pull origin use-ec2-for-signal-r
 cd server/GraphQLDemo/ChatHub
-dotnet build -c Release
+dotnet build -c Release -o out

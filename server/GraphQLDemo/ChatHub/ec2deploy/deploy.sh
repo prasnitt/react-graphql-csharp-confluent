abee0e86dd5e5b62b26d8e583f580cc6bfc96734
Prashant Agrawal <prashant.agrawal@2degrees.nz>
2025-04-25 02:16:56 +1200

    change to main branh

--- server/GraphQLDemo/ChatHub/ec2deploy/deploy.sh
+++ server/GraphQLDemo/ChatHub/ec2deploy/deploy.sh
@@ -4,6 +4,6 @@ set -e  # Ex
 
 cd /home/ec2-user/react-graphql-csharp-confluent
 git reset --hard
-git pull origin use-ec2-for-signal-r
+git pull origin main
 cd server/GraphQLDemo/ChatHub
 dotnet build -c Release -o out
